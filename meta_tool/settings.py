0a9a4818ec4159dace97af76b7533801500e2b61
hendrik <hendrik.huyskens@rl-institut.de>
2018-10-09 14:32:49 +0200

    Moved django secret and debug to environment vars

--- meta_tool/settings.py
+++ meta_tool/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p^mcrnc@+71#8l2fxhh48c+a^gr)w!^v**l78yv!y*&^qi-=99'
+SECRET_KEY = os.environ['DJANGO_SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -36,10 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'django_jsonforms',
     'meta_show',
-    'meta_creator'
+    'meta_creator',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
