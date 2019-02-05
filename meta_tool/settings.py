c6d1049908edd08c16d48c666cf6c479375ab8dc
hendrik <hendrik.huyskens@rl-institut.de>
2019-02-05 14:38:43 +0100

    Added ALLOWED_HOSTS to env

--- meta_tool/settings.py
+++ meta_tool/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', False)
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
 
 
 # Application definition
